a75b3c74f4e2d8adbf10c69b3006489e39ec18f2
Jacek Dziwoki <j.dziwoki@infido.pl>
2017-08-31 22:58:27 +0200

    Publikacja nowej wersji

--- ConnectorRaksSQL/Properties/AssemblyInfo.cs
+++ ConnectorRaksSQL/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConnectorRaksSQL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Synchronizator danych pomiedy RaksSQL i PrestaShop")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("www.INFIDO.pl")]
 [assembly: AssemblyProduct("ConnectorRaksSQL")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1")]
-[assembly: AssemblyFileVersion("1.1")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2")]
