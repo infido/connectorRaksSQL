c1e1a3803be66db192e064691b0fe1b70246fa7c
Jacek Dziwoki <j.dziwoki@infido.pl>
2019-03-20 18:15:21 +0100

    dodanie kolumny kod kreskowy do raportu analizy stanow minimalnych

--- RaportyRaksSQL/OknoRaportow.cs
+++ RaportyRaksSQL/OknoRaportow.cs
@@ -546,11 +546,11 @@ namespace RaportyRaksSQL
             bSaveToRaksSQLClipboard.Enabled = true;
 
             string sql = " ";
-            sql += " select SKROT, NAZWA, sum(STAN_MAG) STAN_MAG, sum(STANMIN) STAN_MIN, sum(STANMAX) STAN_MAX,  ";
+            sql += " select SKROT, KOD_KRESKOWY, NAZWA, sum(STAN_MAG) STAN_MAG, sum(STANMIN) STAN_MIN, sum(STANMAX) STAN_MAX,  ";
             sql += " IIF((sum(STANMIN)-sum(STAN_MAG))<0, ";
             sql += " IIF((sum(STANMAX)-sum(STAN_MAG))<0,(sum(STANMAX)-sum(STAN_MAG)),0),(sum(STANMIN)-sum(STAN_MAG)) ) DO_ZAMOWIENIA,DOST DOSTAWCA, PRODU PRODUCENT, LOKALIZACJA ";
             sql += " FROM ( ";
-            sql += " SELECT GM_TOWARY.SKROT, GM_TOWARY.NAZWA, 0 STAN_MAG, GM_TOWARY.STANMIN, GM_TOWARY.STANMAX, R3DOST.SHORT_NAME DOST, R3PRODU.SHORT_NAME PRODU, GM_LOKALIZACJE.NAZWA LOKALIZACJA ";
+            sql += " SELECT GM_TOWARY.SKROT, GM_TOWARY.KOD_KRESKOWY, GM_TOWARY.NAZWA, 0 STAN_MAG, GM_TOWARY.STANMIN, GM_TOWARY.STANMAX, R3DOST.SHORT_NAME DOST, R3PRODU.SHORT_NAME PRODU, GM_LOKALIZACJE.NAZWA LOKALIZACJA ";
             sql += " FROM GM_TOWARY   ";
             sql += " left join R3_CONTACTS R3DOST on R3DOST.ID=GM_TOWARY.DOSTAWCA ";
             sql += " left join R3_CONTACTS R3PRODU on R3PRODU.ID=GM_TOWARY.PRODUCENT ";
@@ -617,7 +617,7 @@ namespace RaportyRaksSQL
                 sql += " where " + tmpsql;
 
             sql += " union ";
-            sql += " SELECT GM_TOWARY.SKROT , GM_TOWARY.NAZWA, sum(GM_MAGAZYN.ILOSC) STAN_MAG, 0 STANMIN, 0 STANMAX, R3DOST.SHORT_NAME DOST, R3PRODU.SHORT_NAME PRODU, GM_LOKALIZACJE.NAZWA LOKALIZACJA  ";
+            sql += " SELECT GM_TOWARY.SKROT , GM_TOWARY.KOD_KRESKOWY, GM_TOWARY.NAZWA, sum(GM_MAGAZYN.ILOSC) STAN_MAG, 0 STANMIN, 0 STANMAX, R3DOST.SHORT_NAME DOST, R3PRODU.SHORT_NAME PRODU, GM_LOKALIZACJE.NAZWA LOKALIZACJA  ";
             sql += " FROM GM_MAGAZYN ";
             sql += " join GM_TOWARY on GM_TOWARY.ID_TOWARU=GM_MAGAZYN.ID_TOWAR ";
             sql += " join GM_MAGAZYNY on GM_MAGAZYNY.ID=GM_MAGAZYN.MAGNUM ";
@@ -670,8 +670,8 @@ namespace RaportyRaksSQL
             if (tmpsql.Length>0)
                 sql += " where " + tmpsql;
 
-            sql += " group by  GM_MAGAZYN.MAGNUM, GM_TOWARY.SKROT,GM_TOWARY.NAZWA,R3DOST.SHORT_NAME, R3PRODU.SHORT_NAME, GM_LOKALIZACJE.NAZWA) a ";
-            sql += " group by SKROT, NAZWA, DOSTAWCA, PRODUCENT, LOKALIZACJA ";
+            sql += " group by  GM_MAGAZYN.MAGNUM, GM_TOWARY.SKROT, GM_TOWARY.KOD_KRESKOWY, GM_TOWARY.NAZWA,R3DOST.SHORT_NAME, R3PRODU.SHORT_NAME, GM_LOKALIZACJE.NAZWA) a ";
+            sql += " group by SKROT, KOD_KRESKOWY, NAZWA, DOSTAWCA, PRODUCENT, LOKALIZACJA ";
             sql += " order by SKROT ";
 
             FbCommand cdk = new FbCommand(sql, fbconn.getCurentConnection());
