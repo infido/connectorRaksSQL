bae1e86095351fee54bfd461212a62b504cb5dd6
Jacek Dziwoki <j.dziwoki@infido.pl>
2019-12-21 11:11:03 +0100

    poprawa analizy sprzedazy po dostawcach z ceny na rabat

--- RaportyRaksSQL/OknoRaportow.cs
+++ RaportyRaksSQL/OknoRaportow.cs
@@ -463,7 +463,7 @@ namespace RaportyRaksSQL
             //sql += " 0 DO_ZAMOWIENIA, ";
             sql += " R3DOST.SHORT_NAME DOSTAWCA, ";
             sql += " R3PRODU.SHORT_NAME PRODUCENT, ";
-            sql += " GM_CENY.NAZWA RODZAJ";
+            sql += " GM_RABATY.NAZWA RODZAJ";
             sql += " from GM_TOWARY";
             sql += " left join GM_FSPOZ on GM_TOWARY.ID_TOWARU=GM_FSPOZ.ID_TOWARU ";
             sql += " left join GM_WZPOZ on GM_FSPOZ.ID = GM_WZPOZ.ID_FSPOZ";
@@ -471,7 +471,7 @@ namespace RaportyRaksSQL
             sql += " left join GM_MAGAZYNY on GM_FS.MAGNUM=GM_MAGAZYNY.ID ";
             sql += " left join R3_CONTACTS R3DOST on R3DOST.ID=GM_TOWARY.DOSTAWCA ";
             sql += " left join R3_CONTACTS R3PRODU on R3PRODU.ID=GM_TOWARY.PRODUCENT ";
-            sql += " left join GM_CENY on GM_CENY.ID = GM_TOWARY.RABAT ";
+            sql += " left join GM_RABATY on GM_RABATY.ID = GM_TOWARY.RABAT ";
 
             if (podstawoweGT.Length != 0)
                 sql += " left join GM_GRUPYT on GM_GRUPYT.ID=GM_TOWARY.GRUPA";
@@ -510,7 +510,7 @@ namespace RaportyRaksSQL
             //sql += " 0 DO_ZAMOWIENIA, ";
             sql += " R3DOST.SHORT_NAME DOSTAWCA, ";
             sql += " R3PRODU.SHORT_NAME PRODUCENT, ";
-            sql += " GM_CENY.NAZWA RODZAJ";
+            sql += " GM_RABATY.NAZWA RODZAJ";
             sql += " from GM_TOWARY";
             sql += " left join GM_KSPOZ on GM_TOWARY.ID_TOWARU=GM_KSPOZ.ID_TOWARU ";
             //sql += " left join GM_WZPOZ on GM_KSPOZ.ID = GM_WZPOZ.ID_KSPOZ";
@@ -518,7 +518,7 @@ namespace RaportyRaksSQL
             sql += " left join GM_MAGAZYNY on GM_KS.MAGNUM=GM_MAGAZYNY.ID ";
             sql += " left join R3_CONTACTS R3DOST on R3DOST.ID=GM_TOWARY.DOSTAWCA ";
             sql += " left join R3_CONTACTS R3PRODU on R3PRODU.ID=GM_TOWARY.PRODUCENT ";
-            sql += " left join GM_CENY on GM_CENY.ID = GM_TOWARY.RABAT ";
+            sql += " left join GM_RABATY on GM_RABATY.ID = GM_TOWARY.RABAT ";
 
             if (podstawoweGT.Length != 0)
                 sql += " left join GM_GRUPYT on GM_GRUPYT.ID=GM_TOWARY.GRUPA";
