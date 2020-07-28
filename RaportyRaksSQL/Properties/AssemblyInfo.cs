23b17dee20f37c3de9bab4dd6f3631efaaf82211
Jacek Dziwoki <jacek.dziwoki@gmail.com>
2020-07-28 13:28:42 +0200

    Wersja 2.9

--- RaportyRaksSQL/Properties/AssemblyInfo.cs
+++ RaportyRaksSQL/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RaportyRaksSQL")]
 [assembly: AssemblyDescription("Mechanizm raportowania z RaksSQL")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("www.INFIDO.pl")]
+[assembly: AssemblyCompany("Jacek Dziwoki")]
 [assembly: AssemblyProduct("RaportyRaksSQL")]
-[assembly: AssemblyCopyright("Copyright ©  2016 -2017")]
+[assembly: AssemblyCopyright("Copyright ©  2016 -2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.8")]
-[assembly: AssemblyFileVersion("2.8")]
+[assembly: AssemblyVersion("2.9")]
+[assembly: AssemblyFileVersion("2.9")]
